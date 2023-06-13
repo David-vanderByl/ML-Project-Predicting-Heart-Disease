c2009681a7a83028f0e76fe87058fa05f5a4cf6a
David van der Byl <david.vanderbyl94@gmail.com>
2023-06-13 17:35:04 +1200

    updating markdown converter

--- ipynb_to_markdown.py
+++ ipynb_to_markdown.py
@@ -12,13 +12,23 @@ with open(ipynb_file, 'r', encoding='utf-8') as f:
 exporter = MarkdownExporter()
 exporter.exclude_output_prompt = False  # Include code cell prompts
 exporter.exclude_input = True  # Exclude code cell inputs
-exporter.exclude_output = False  # Include code cell outputs
+exporter.exclude_output = True  # Include code cell outputs
+
+# Add styling information to the notebook metadata
+notebook['metadata']['jupyter']['nbconvert']['execute_notebooks'] = 'auto'
+notebook['metadata']['jupyter']['widgets'] = {
+    'widget_state': {},
+    'application/vnd.jupyter.widget-view+json': {},
+    'version_major': 2,
+    'version_minor': 0
+}
 
 # Convert the notebook to Markdown
-markdown, _ = exporter.from_notebook_node(notebook)
+markdown, _ = exporter.from_notebook_node(notebook, resources={})
 
 # Save the Markdown content to a file
 markdown_file = 'markdown.md'
 with open(markdown_file, 'w', encoding='utf-8') as f:
     f.write(markdown)
+
 
